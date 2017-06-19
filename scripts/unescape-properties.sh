8fae3360415b41e4903fb66e49b8ad624416e005
Michal Stanke <mstanke@mozilla.cz>
2017-06-19 19:39:00 +0200

    Make sure colons are escaped in output keys

--- scripts/unescape-properties.sh
+++ scripts/unescape-properties.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# run both for the input and output to unescape colons, equal signs and exclamation marks
+# run for output to unescape colons, equal signs, exclamation marks and tabulators
 find . -type f -name "*.properties" -exec sed -i 's/\\:/\:/g' {} +
 find . -type f -name "*.properties" -exec sed -i 's/\\=/\=/g' {} +
 find . -type f -name "*.properties" -exec sed -i 's/\\!/\!/g' {} +
 find . -type f -name "*.properties" -exec sed -i 's/\\t/\t/g' {} +
+
+# escape colons in .properties keys
+./escape-input.sh
