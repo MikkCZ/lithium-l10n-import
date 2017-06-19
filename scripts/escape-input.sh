d08e5ea6ea507f0aec3441c7f73aa2b9e4705900
Michal Stanke <mstanke@mozilla.cz>
2017-06-19 19:19:41 +0200

    Escape colons in keys only

--- scripts/escape-input.sh
+++ scripts/escape-input.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+# escape colons in .properties file keys
+function _escape_colons_in_keys {
+    file=$1;
+    grep -n ':' $file | while read -r line ; do
+        k=$( echo "$line" | cut -d'=' -f1 | xargs );
+        line_num=$( echo "$k" | cut -d':' -f1 );
+        key=$( echo "$k" | cut -d':' -f2-9999 );
+        if [[ $key == *':'* ]]; then
+            newkey="$(sed s/\\:/\\\\\\\\:/g <<< $key)";
+            sed -i $line_num"s/$key/$newkey/g" $file;
+        fi
+    done
+}
+
+export -f _escape_colons_in_keys;
+
 # run in the both source and input folder to escape colons in property keys
-find . -type f -name "*.properties" -exec sed -i 's/\:/\\:/g' {} +
+find . -type f -name "*.properties" -exec bash -c '_escape_colons_in_keys "$0"' {} \;
